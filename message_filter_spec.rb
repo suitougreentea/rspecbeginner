502176528763bd8de675319506ab7637ff76691a
suitougreentea <suitougreentea@users.noreply.github.com>
2014-07-24 17:15:16 +0900

    use rspec/its

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,4 +1,5 @@
 require_relative 'message_filter'
+require 'rspec/its'
 
 describe MessageFilter do
   shared_examples 'MessageFilter with argument "foo"' do
@@ -9,9 +10,7 @@ describe MessageFilter do
   context 'with argument "foo"' do
     subject { MessageFilter.new('foo') }
     it_behaves_like 'MessageFilter with argument "foo"'
-    it 'ng_words should not be empty' do
-      expect(subject.ng_words).not_to be_empty
-    end
+    its(:ng_words) { is_expected.not_to be_empty }
   end
 
   context 'with argument "foo","bar"' do
